c8fb49a0e4f6387a7f272da125e484d2dda392b3
tchappel <tchappel@ebay.com>
2020-02-04 14:52:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
+import Sublist from "./Sublist"
 
-function App() {
+const editor = new EditorJS({
+  /**
+   * Id of Element that should contain Editor instance
+   */
+  holderId: "codex-editor",
+  tools: {
+    header: Header,
+    list: {
+      class: List,
+      inlineToolbar: true
+    },
+    sublist: {
+      class: Sublist,
+      inlineToolbar: true
+    },
+  },
+  onReady: () => {
+    console.log("Editor.js is ready to work!");
+  },
+  onChange: () => {
+    console.log("Now I know that Editor's content changed!");
+  },
+  data: {
+    blocks: [
+      {
+        type: "header",
+        data: {
+          text: "My JS Editor",
+          level: 1
+        }
+      },
+      {
+        type: "paragraph",
+        data: {
+          text: "This is a list",
+        }
+      },
+      {
+        type: "list",
+        data: {
+          style: "unordered",
+          items: [
+              'list item 1!',
+              'list item 2',
+              'list item 3',
+          ]
+        }
+      }
+    ]
+  }
+});
+
+const saveData = () => {
+  editor
+    .save()
+    .then(outputData => {
+      console.log("Article data: ", JSON.stringify(outputData));
+    })
+    .catch(error => {
+      console.log("Saving failed: ", error);
+    });
+};
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "#F6F8FA" }}>
+      <div id="codex-editor"></div>
+      <button onClick={saveData}>Save</button>
     </div>
   );
-}
-
-export default App;
+};
